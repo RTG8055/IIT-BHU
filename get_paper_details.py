fed5e56f440f71ffd1e763c76b1d3323db492018
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-11-11 11:22:25 +0530

    optimized the search

--- get_paper_details.py
+++ get_paper_details.py
@@ -2,7 +2,25 @@ import zipfile
 import json
 import re
 
-file = open("IR_paperIDs",'r')
+
+def bsearch(id,all,k,l):
+	if(k<l):
+		mid = k + (l-k)/2
+		# print k,l,mid
+		# print id,all[mid]
+		if(id == all[mid]):
+			return 1;
+		elif(id < all[mid]):
+			l = mid
+			return bsearch(id,all,k,l)
+		elif(id>all[mid]):
+			k=mid+1
+			return bsearch(id,all,k,l)
+	else:
+		return 0;
+
+
+file = open("IR_IDs_sorted",'r')
 all_pid=[]
 for line in file:
 	all_pid.append(line[:-1])
@@ -13,20 +31,22 @@ print all_pid
 with zipfile.ZipFile("zips/Papers.zip") as z:
 	with z.open("Papers.txt") as f:
 		i=0
+		k=0
 		for line in f:
 			i+=1
-			if(i==1001):
-				break
+			# if(i==100001):
+				# break
 			l = line.split('\t')
 			print "checking paper number ",i
 			x=[]
 			# print l[0]
-
-			if (l[0] in all_pid):
+			if(bsearch(l[0],all_pid,0,len(all_pid)-1) == 1):
+			# if (l[0] in all_pid):
+				k=k+1
 				x.append(l[1])
 				x.append(l[3])
 				x.append(l[6])
-				print i, "matched paper id", l[0]
+				print k, "matched paper id", l[0]
 				if (l[8]== ''):
 					x.append(l[9])
 				else:
@@ -39,6 +59,8 @@ with zipfile.ZipFile("zips/Papers.zip") as z:
 					new.write(j)
 					new.write('\t')
 				new.write('\n')
+			# if(l[0] == '7ED39B38'):
+				# break
 
 # for k,v in p_det.items():
 # 	new.write(k)
