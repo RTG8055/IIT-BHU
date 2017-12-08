a9cf3bb8190d0f32084214382c2ac3065575393d
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-12-08 18:43:17 +0530

    upadted PAperKeywords PAth

--- get_groups_paperIDs_keywords.py
+++ get_groups_paperIDs_keywords.py
@@ -37,63 +37,62 @@ for line in G:
 # print groupno_parentID
 # check whether a paper belonging to Beta ACP is there in chemistry or not where beta ACP is not a part
 
-with zipfile.ZipFile("../../../MAGNEW/PaperKeywords.zip") as z:
-	with z.open("PaperKeywords.txt") as f:
-		i=0
-		for line in f:
-			i+=1
-			# print line
-			pid,keyword,fid = line.split('\t')
-			# print pid,keyword,fid
-			groupno=0
-			while(groupno<len(groups)):
-				# print fid,groupno
-				# print "\n"
-				if(bsearch(fid,groups[groupno],0,len(groups[groupno])-1)):
-					# print "found" + fid, groupno
-					parentID = groupno_parentID[groupno]
-					if(groups_with_paperIDs_keywords.get(parentID) == None):
-						c=[[],[]]
-						c[0].append(pid)
-						c[1].append(keyword)
-						groups_with_paperIDs_keywords[parentID] = c
-					else:
-						x=groups_with_paperIDs_keywords[parentID]
-						paperAlreadyThere=False
-						keywordAlreadyThere=False
-						for d in [0,1]:
-							h=0
-							for j in x[d]:
-								h+=1
-								#h==1 implies papers are checked
-								#h==2 implies keywords are checked
-								if(h==1):
-									if(j.strip() == pid):
-										paperAlreadyThere=True
-										break
-								if(h==2):
-									if(j.strip() == keyword):
-										keywordAlreadyThere = True
-						if(not paperAlreadyThere):
-							temp = groups_with_paperIDs_keywords[parentID][0] #get the list of papers in that group
-							temp.append(pid)
-							groups_with_paperIDs_keywords[parentID][0]=temp #update the paperIDs list
-							print "added paper-"+pid +" to " + parentID + " Group"
+with open("../../../MAGNEW/PaperKeywords/PaperKeywords.txt") as f:
+	i=0
+	for line in f:
+		i+=1
+		# print line
+		pid,keyword,fid = line.split('\t')
+		# print pid,keyword,fid
+		groupno=0
+		while(groupno<len(groups)):
+			# print fid,groupno
+			# print "\n"
+			if(bsearch(fid,groups[groupno],0,len(groups[groupno])-1)):
+				# print "found" + fid, groupno
+				parentID = groupno_parentID[groupno]
+				if(groups_with_paperIDs_keywords.get(parentID) == None):
+					c=[[],[]]
+					c[0].append(pid)
+					c[1].append(keyword)
+					groups_with_paperIDs_keywords[parentID] = c
+				else:
+					x=groups_with_paperIDs_keywords[parentID]
+					paperAlreadyThere=False
+					keywordAlreadyThere=False
+					for d in [0,1]:
+						h=0
+						for j in x[d]:
+							h+=1
+							#h==1 implies papers are checked
+							#h==2 implies keywords are checked
+							if(h==1):
+								if(j.strip() == pid):
+									paperAlreadyThere=True
+									break
+							if(h==2):
+								if(j.strip() == keyword):
+									keywordAlreadyThere = True
+					if(not paperAlreadyThere):
+						temp = groups_with_paperIDs_keywords[parentID][0] #get the list of papers in that group
+						temp.append(pid)
+						groups_with_paperIDs_keywords[parentID][0]=temp #update the paperIDs list
+						print "added paper-"+pid +" to " + parentID + " Group"
 
 
-						if(not keywordAlreadyThere):
-							temp = groups_with_paperIDs_keywords[parentID][1] # get the list if keywords in that group
-							temp.append(keyword)
-							groups_with_paperIDs_keywords[parentID][1]= temp # update the Keywords list
-							print "added keyword-"+keyword +" to " + parentID + "Group"
+					if(not keywordAlreadyThere):
+						temp = groups_with_paperIDs_keywords[parentID][1] # get the list if keywords in that group
+						temp.append(keyword)
+						groups_with_paperIDs_keywords[parentID][1]= temp # update the Keywords list
+						print "added keyword-"+keyword +" to " + parentID + "Group"
 
-				# print "abcd",groupno
-				groupno+=1
-				# if(groupno==443):
-					# break
-			print "line " + str(i) + " done"
-			# if(i==1000):
+			# print "abcd",groupno
+			groupno+=1
+			# if(groupno==443):
 				# break
+		print "line " + str(i) + " done"
+		# if(i==1000):
+			# break
 
 i=0
 for k, v in groups_with_paperIDs_keywords.items():
