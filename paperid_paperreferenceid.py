b315cec79ff8f9c8267dfdebf916cbb07d4a0c28
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-12-10 09:14:25 +0530

    update paper reference script for path

--- paperid_paperreferenceid.py
+++ paperid_paperreferenceid.py
@@ -4,23 +4,24 @@ import zipfile
 
 x={}
 enm=open("Paperid_paperrefid.txt",'w')
-with zipfile.ZipFile("zips/PaperReferences.zip") as z:
-	with z.open("PaperReferences.txt",'r') as f:
-		i=0
-		c=[]
-		for line in f:
-			i+=1
-			line=line.decode("utf-8")
-			l=line.split('\t')
-			l[0]=l[0].strip()
-			l[1]=l[1].strip()
-			if(x.get(l[0])==None):
-				x[l[0]]=[]
-				x[l[0]].append(0)
-			else:
-				x[l[0]][0]+=1
-				x[l[0]].append(l[1])
-			print(i)
+# with zipfile.ZipFile("zips/PaperReferences.zip") as z:
+	# with z.open("PaperReferences.txt",'r') as f:
+with open("../../../MAGNEW/PaperKeywords/PaperKeywords.txt") as f:
+	i=0
+	c=[]
+	for line in f:
+		i+=1
+		line=line.decode("utf-8")
+		l=line.split('\t')
+		l[0]=l[0].strip()
+		l[1]=l[1].strip()
+		if(x.get(l[0])==None):
+			x[l[0]]=[]
+			x[l[0]].append(0)
+		else:
+			x[l[0]][0]+=1
+			x[l[0]].append(l[1])
+		print(i)
 y=0
 for v,k in x.items():
 	if(k[0]==0):
