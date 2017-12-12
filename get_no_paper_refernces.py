b3d0bb71ec99d6556b9fac58310782beee560d8d
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-12-12 15:36:33 +0530

    work on server no_ref

--- get_no_paper_refernces.py
+++ get_no_paper_refernces.py
@@ -4,28 +4,30 @@ import zipfile
 
 
 papers = {}
-with zipfile.ZipFile("zips/PaperReferences.zip") as z:
-	with z.open("PaperReferences.txt") as f:
-		i=0
-		for line in  f:
-			i+=1
-			# print line
-			pid,refID = line.split('\t')
-			refID = refID.strip('\r|\n')
-			# print pid,repr(refID)
+# with zipfile.ZipFile("zips/PaperReferences.zip") as z:
+	# with z.open("PaperReferences.txt") as f:
+with open("../../../MAGNEW/PaperReferences/PaperReferences.txt") as f:
 
-			if(papers.get(pid) == None):
-				c=[1]
-				c.append(refID)
-				papers[pid] =c
-			else:
-				temp = papers.get(pid)
-				temp[0]+=1
-				temp.append(refID)
-			if(i%100000 == 0):
-				print "line " + str(i)
-			# if(i==1000000):
-				# break
+	i=0
+	for line in  f:
+		i+=1
+		# print line
+		pid,refID = line.split('\t')
+		refID = refID.strip('\r|\n')
+		# print pid,repr(refID)
+
+		if(papers.get(pid) == None):
+			c=[1]
+			c.append(refID)
+			papers[pid] =c
+		else:
+			temp = papers.get(pid)
+			temp[0]+=1
+			temp.append(refID)
+		if(i%100000 == 0):
+			print "line " + str(i)
+		# if(i==1000000):
+			# break
 
 new = open("paper_no_refernces_refernces.txt",'w')
 
