587cb5d2e662807f4e7c26786ecf4461b768a92f
Rahul <rahul.agarwal4@learner.manipal.edu>
2017-12-18 10:00:31 +0530

    get cs urls_server

--- only_CS_files/get_cs_paper_urls.py
+++ only_CS_files/get_cs_paper_urls.py
@@ -17,36 +17,37 @@ file.close()
 
 cs_paper_url={}
 
-with zipfile.ZipFile("../zips/PaperUrls.zip") as z:
-	with z.open("PaperUrls.txt",'r') as f:
-		i=0
-		j=0
-		for line in f:
-			i+=1
-			pid,url = line.split('\t')
-			url = url.strip("\n|\r")
-			if pid in papers:
-				j+=1
-				# print repr(pid),repr(url)
-				# print cs_paper_url.get(pid)
-				if(cs_paper_url.get(pid) == None):
-					# print [url]
-					cs_paper_url[pid] = [url]
-					# print cs_paper_url
-				else:
-					c =cs_paper_url[pid]
-					# print c
-					c.append(url)
-					cs_paper_url[pid] = c
-					# print cs_paper_url
-			# if(i==10):
-				# break
-			if(i%1000000==0):
-				print i
+# with zipfile.ZipFile("../zips/PaperUrls.zip") as z:
+# 	with z.open("PaperUrls.txt",'r') as f:
+with open("../../../MAGNEW/PaperUrls/PaperUrls.txt") as f:
+	i=0
+	j=0
+	for line in f:
+		i+=1
+		pid,url = line.split('\t')
+		url = url.strip("\n|\r")
+		if pid in papers:
+			j+=1
+			# print repr(pid),repr(url)
+			# print cs_paper_url.get(pid)
+			if(cs_paper_url.get(pid) == None):
+				# print [url]
+				cs_paper_url[pid] = [url]
+				# print cs_paper_url
+			else:
+				c =cs_paper_url[pid]
+				# print c
+				c.append(url)
+				cs_paper_url[pid] = c
+				# print cs_paper_url
+		# if(i==10):
+			# break
+		if(i%1000000==0):
+			print i
 
 print i,j
 
-new = open("cs_papers_urls.txt",'w')
+new = open("upload_cs_papers_urls.txt",'w')
 i=0
 for k,v in cs_paper_url.items():
 	i+=1
