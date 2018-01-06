0c58e37fbedb7938f33a4ee68812ba4e9cba614e
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-01-06 18:51:04 +0530

    final details script

---
+++ Result metric -II/final_details.py
@@ -0,0 +1,34 @@
+paperids={}
+papers=set()
+with open("output2/sample6/final_papers6_1.txt",'r') as f:
+	i=0
+	for line in f:
+		i+=1
+		line = line.strip('\n|\r')
+		# print repr(line)
+		paperids[line] = i
+		papers.add(line)
+
+print i,len(paperids)
+
+all_titles = {}
+all_journals={}
+
+f = open("output2/sample6/papers6_titles_journals.txt",'r')
+for line in f:
+	paperID,pJournaltitle = line.split('\t')
+	pjournal,ptitle = pJournaltitle.split("<--")
+	ptitle = ptitle.strip('\n')
+	all_titles[paperID] = ptitle
+	all_journals[paperID] = pjournal
+f.close()
+
+
+# print all_titles
+new = open("output2/sample6/final_details_1.txt",'w')
+i=0
+for p in paperids:
+	i+=1
+	new.write(p + "\t" + all_titles[p] + "\t" + all_journals[p] + "\n")
+
+print i
