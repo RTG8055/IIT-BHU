1ef50f3fdd5637311576a1e238276518d2271827
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-01-27 16:13:14 +0530

    Graphs for ndcg and precision

--- result-metric-I/graph.py
+++ result-metric-I/graph.py
@@ -3,7 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-all_sheets = pd.ExcelFile("PDS and AI.xlsx")
+# file_name = "Final"
+file_name = "Precision"
+
+all_sheets = pd.ExcelFile(file_name+".xlsx")
+
+# plt.rcParams["font.weight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"
+plt.rcParams.update({'figure.autolayout': True})
+
+all_fig = plt.figure(figsize=(11.93,15.98))
+
+# ax = fig.add_axes([0.1,0.1,0.8,0.8])
+# plt.text()
+
+# ax.legend()
+subplot_no=0
 print all_sheets.sheet_names
 for sheet in all_sheets.sheet_names:
 	print sheet
@@ -12,30 +27,57 @@ for sheet in all_sheets.sheet_names:
 	df = all_sheets.parse(sheet)
 	print df.head()
 	plt.style.use('seaborn-bright')
-	fig = plt.figure(figsize=(10,10))
-	ax = fig.add_axes([0.1,0.1,0.8,0.8])
-	l1=ax.plot(np.arange(1,21,1),df.Snaver,alpha=0.7,color='blue',marker='s')
-	l2=ax.plot(np.arange(1,21,1),df.Springer,ls=(0,(5,5,)),alpha=0.7,color='green',marker='8')
-	l3=ax.plot(np.arange(1,21,1),df.Elsevier,ls=(0, (3,1,5,1,5,5)),alpha=0.7,color='red',marker='+')
+	# ax = fig.add_axes([0.1,0.1,0.8,0.8])
+	print subplot_no
+	subplot_no+=1
+	if(subplot_no<7):
+		continue
+	# if(subplot_no==7):
+		# break
+	ax = plt.subplot(4,3,subplot_no)
+
+	# plt.text()
+	l1=ax.plot(np.arange(1,21,1),df.Snaver,alpha=0.7,color='blue',marker='s',markersize=6)
+	l2=ax.plot(np.arange(1,21,1),df.Springer,ls=(0,(5,5,)),alpha=0.7,color='green',marker='8',markersize=6)
+	l3=ax.plot(np.arange(1,21,1),df.Elsevier,ls=(0, (3,1,5,1,5,5)),alpha=0.7,color='red',marker='+',markersize=6)
+
 	ax.legend()
-	ax.set_xticks(np.arange(1,21,1))
+	ax.set_xticks(np.arange(0,21,2))
 	# plt.axis([0.99,20.01,0.59,1.01])
 	ax.set_xlabel("Top K papers")
-	ax.set_ylabel("nDCG@K")
+	# ax.set_ylabel("nDCG@K")
+	ax.set_ylabel("Avg Precision@K")
 	name =""
 	if(sheet == "IR"):
 		name = "Information Retrieval"
 	elif(sheet == "WSN"):
 		name = "Wireless Networks"
-	elif(sheet == "PDS_Graph"):
+	elif(sheet == "PDS"):
 		name = "Parallel and distributed systems"
-		sheet = "PDS"
-	elif(sheet == "AI_Graph"):
+		# sheet = "PDS"
+	elif(sheet == "AI"):
 		name = "Artificial Intellignece"
-		sheet ="AI"
+		# sheet ="AI"
+	elif(sheet == "CV"):
+		name = "Computer Vision"
+	elif(sheet == "mm"):
+		name = "Multimedia"
+	elif(sheet == "Security"):
+		name = "Security"
+	elif(sheet == "DM"):
+		name = "Data Mining"
+	elif(sheet == "NLP"):
+		name = "Natural Language Processing"
+	elif(sheet == "ML"):
+		name = "Machine Learning"
+	elif(sheet == "IP"):
+		name = "Image Processing"
 	else:
 		name = "Software Engineering"
-	
-	ax.set_title(name)
+	ax.set_title(name,fontweight = "bold")
 	# sns.despine()
-	plt.savefig(sheet + "8.png",dpi=300)
+	# plt.tight_layout()
+
+
+plt.savefig(file_name + "7.eps",dpi=300, bbox_inches = "tight")
+plt.savefig(file_name + "7.png",dpi=300, bbox_inches = "tight")
