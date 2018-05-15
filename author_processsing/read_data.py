d34302df62792cb5c4aee2c09393f1301fd18f94
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-05-15 09:18:44 +0530

    asdf

--- author_processsing/read_data.py
+++ author_processsing/read_data.py
@@ -12,25 +12,26 @@ with zipfile.ZipFile("../only_CS_files/upload_cs_paperids2.zip") as z:
 
 print len(cs_paperids),i
 author_papers={}
-with zipfile.ZipFile("../zips/PaperAuthorAffiliations.zip") as z:
-	with z.open("PaperAuthorAffiliations.txt") as f:
-		i=0
-		for line in f:
-			#paperid, authorid, affiliationid,original affiliation name, normalized, author sequence number
-			# print line
-			pid,aid,afid,_o,_n,asn = line.strip('\r|\n').split('\t')
-			# print repr(pid),repr(aid),repr(asn)
-			i+=1
-			if(i%1000000 ==0):
-				# print i
-				break
-			if(pid in cs_paperids):
-				if(author_papers.get(aid) == None):
-					author_papers[aid] = [(pid,asn)]
-				else:
-					author_papers[aid].append((pid,asn))
+# with zipfile.ZipFile("../zips/PaperAuthorAffiliations.zip") as z:
+# 	with z.open("PaperAuthorAffiliations.txt") as f:
+with open("../../../MAGNEW/PaperAuthorAffiliations/PaperAuthorAffiliations.txt") as f:
+	i=0
+	for line in f:
+		#paperid, authorid, affiliationid,original affiliation name, normalized, author sequence number
+		# print line
+		pid,aid,afid,_o,_n,asn = line.strip('\r|\n').split('\t')
+		# print repr(pid),repr(aid),repr(asn)
+		i+=1
+		if(i%1000000 ==0):
+			# print i
+			break
+		if(pid in cs_paperids):
+			if(author_papers.get(aid) == None):
+				author_papers[aid] = [(pid,asn)]
 			else:
-				continue
+				author_papers[aid].append((pid,asn))
+		else:
+			continue
 print len(author_papers),i
 
 new = open('author_papers.txt','w')
