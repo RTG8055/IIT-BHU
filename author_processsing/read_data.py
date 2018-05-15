0a1fca649134345f595db536e2a598d8d6eb5994
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-05-15 08:56:07 +0530

    author update

--- author_processsing/read_data.py
+++ author_processsing/read_data.py
@@ -1,6 +1,16 @@
 import zipfile
 
+cs_paperids=set()
 
+with zipfile.ZipFile("../only_CS_files/upload_cs_paperids2.zip") as z:
+	with z.open("cs_paperids2.txt") as f:
+		i=0
+		for line in f:
+			i+=1
+			line = line.strip("\r|\n")
+			cs_paperids.add(line)
+
+print len(cs_paperids),i
 author_papers={}
 with zipfile.ZipFile("../zips/PaperAuthorAffiliations.zip") as z:
 	with z.open("PaperAuthorAffiliations.txt") as f:
@@ -8,16 +18,20 @@ with zipfile.ZipFile("../zips/PaperAuthorAffiliations.zip") as z:
 		for line in f:
 			#paperid, authorid, affiliationid,original affiliation name, normalized, author sequence number
 			# print line
-			pid,aid,_a,_o,_n,asn = line.strip('\r|\n').split('\t')
+			pid,aid,afid,_o,_n,asn = line.strip('\r|\n').split('\t')
 			# print repr(pid),repr(aid),repr(asn)
 			i+=1
-			if(i%100000 ==0):
-				print i
-			if(author_papers.get(aid) == None):
-				author_papers[aid] = [(pid,asn)]
+			if(i%1000000 ==0):
+				# print i
+				break
+			if(pid in cs_paperids):
+				if(author_papers.get(aid) == None):
+					author_papers[aid] = [(pid,asn)]
+				else:
+					author_papers[aid].append((pid,asn))
 			else:
-				author_papers[aid].append((pid,asn))
-# print author_papers
+				continue
+print len(author_papers),i
 
 new = open('author_papers.txt','w')
 i=0
@@ -27,8 +41,8 @@ for k,v in author_papers.items():
 	# print k,v
 	# text file format
 	# author id 	list of (paper, sequence number)
-	new.write(str(k)+ '\t'+ str(' '.join([x[0] + '-' + x[1] for x in v]))  + '\n')
-print len(author_papers)
+	new.write(str(k)+ '\t'+ str(len(v))+ '\t' + str(' '.join([x[0] + '-' + x[1] for x in v]))  + '\n')
+print len(author_papers),i
 new.close()
 
 
--- dblp/read_dataset.py
+++ dblp/read_dataset.py
@@ -2,6 +2,7 @@ import zipfile,os,json
 
 path = 'datasets/dblp/dblp_v10.zip/dblp-ref/'
 with zipfile.ZipFile("../datasets/dblp/dblp_v10.zip") as z:
-	z.extract('dblp-ref/')
-	z.r
+	with z.open("dblp-ref/dblp-ref-0.json") as f:
+		i=0
+		data = json.load(f)
 		
