85e09976c900935df995a97ef7917f98076e5051
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-05-28 23:03:42 +0530

    coauthor:

--- author_processsing/co_author_network.py
+++ author_processsing/co_author_network.py
@@ -1,56 +1,60 @@
 # import networkx
-import zipfile
-import psutil
+# import zipfile
+# import psutil
 
 co_authors = {}
 paper_authors = {}
-author_list=[]
-print psutil.virtual_memory()
+# print psutil.virtual_memory()
 
-with zipfile.ZipFile("no_upload_paper_authors.zip") as z:
-	with z.open("paper_authors.txt") as f:
-		i=0
-		for line in f:
-			i+=1
-			pid,no_authors,authors = line.strip('\r|\n').split("\t")
-			authors = authors.split(' ')
-			if(i%1000000==0):
-				print i
-				print psutil.virtual_memory()
-				# break
-			if(no_authors < 2):
-				print pid
+# with zipfile.ZipFile("no_upload_paper_authors.zip") as z:
+# 	with z.open("paper_authors.txt") as f:
+with open("paper_authors.txt") as f:
+	i=0
+	for line in f:
+		i+=1
+		pid,no_authors,authors = line.strip('\r|\n').split("\t")
+		authors = authors.split(' ')
+		if(i%1000000==0):
+			print i
+			# print psutil.virtual_memory()
+			# break
+		# if(no_authors < 2):
+			# print pid
+			# continue
+		paper_authors[pid] = authors
+
+		# paper_authors[pid] = i
+		# author_list.append(authors)
+	print i
+# print psutil.virtual_memory()
+
+
+# with zipfile.ZipFile("no_upload_author_papers.zip") as z:
+	# with z.open("author_papers.txt") as f:
+with open("author_papers.txt") as f:
+	i=0
+	for line in f:
+		i+=1
+		aid,no_papers,papers = line.strip('\r|\n').split('\t')
+		papers = papers.strip("[|]|\n|\r")
+		papers = papers.split(' ')
+		paperids = [p.split('-')[0] for p in papers]
+		co_authors[aid] = set()
+		if(i%1000000==0):
+			print i
+			# break
+		for p in paperids:
+			if(paper_authors[p] == None):
 				continue
-			paper_authors[pid] = authors
-			
-			# paper_authors[pid] = i
-			# author_list.append(authors)
-		print i
-print psutil.virtual_memory()
-
-
-with zipfile.ZipFile("no_upload_author_papers.zip") as z:
-	with z.open("author_papers.txt") as f:
-		i=0
-		for line in f:
-			i+=1
-			aid,no_papers,papers = line.strip('\r|\n').split('\t')
-			papers = papers.strip("[|]|\n|\r")
-			papers = papers.split(' ')
-			paperids = [p.split('-')[0] for p in papers]
-			co_authors[aid] = set()
-			if(i%1000==0):
-				print i
-				break
-			for p in paperids:
+			else:
 				co_authors[aid].add(set(paper_authors[p]))
-			print psutil.virtual_memory()
+		# print psutil.virtual_memory()
 
 
 
-		print i,total,psutil.virtual_memory()
+	print i,total
 
-print co_authors,len(co_authors),psutil.virtual_memory()
+print co_authors,len(co_authors)
 
 paper_authors.clear()
 
