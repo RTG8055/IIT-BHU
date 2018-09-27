c6c5e7a25f3804967441e3c59cfd7740544e86c4
Rahul <rahul.agarwal4@learner.manipal.edu>
2018-09-27 14:25:27 +0530

    some redone graphs

--- result-metric-I/new_graph.py
+++ result-metric-I/new_graph.py
@@ -2,15 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
+import matplotlib.font_manager as font_manager
 # file_name = 'new_data'
 
 file_name= 'Updated_Resultsnew'
 
 all_sheets = pd.ExcelFile(file_name + '.xlsx')
-
+hfont = {'fontname':'Helvetica'}
 plt.rcParams['axes.labelweight'] = 'bold'
 plt.rcParams.update({'figure.autolayout' : True})
+font = font_manager.FontProperties(family='Times New Roman',
+                                   style='normal',size = 9)
 
 for sheet in all_sheets.sheet_names:
 	if('Sheet' in  sheet):
@@ -29,7 +31,7 @@ for sheet in all_sheets.sheet_names:
 	df = all_sheets.parse(sheet)
 	print df.head()
 
-	fig = plt.figure(figsize=(6,4))
+	fig = plt.figure(figsize=(7.5,4))
 	plt.style.use('seaborn-bright')
 
 	colors=['red','black','green','orange','aqua','magenta','brown','blue']
@@ -45,11 +47,12 @@ for sheet in all_sheets.sheet_names:
 	for i in range(0,len(cols)):
 		plt.plot(np.arange(1,nrows+1,1),df[cols[i]],alpha=0.6,markersize=6,color=colors[i],marker=markers[i])
 		
-	plt.xlabel(xlabel)
+	plt.xlabel(xlabel,**hfont)
 	plt.ylabel(ylabel)
 	plt.xticks(np.arange(0,nrows+1,tick_skip))
-	plt.legend(bbox_to_anchor=(0.48,-0.67), loc="lower left", bbox_transform=fig.transFigure,ncol=len(cols), mode="expand",labelspacing = 0.,borderaxespad=17.3,frameon=False)
+	plt.legend(bbox_to_anchor=(0.52,-0.72), loc="lower left", bbox_transform=fig.transFigure,ncol=len(cols), mode="expand",labelspacing = .0,frameon=False,borderaxespad=22.2,prop=font,handletextpad=0.1)
 	# plt.show()
-	plt.savefig(file_name + "_"+sheet + ".eps",dpi=300, bbox_inches = "tight")
+	plt.savefig(file_name + "3_"+sheet + ".eps",dpi=300, bbox_inches = "tight")
+	# break
 
 
