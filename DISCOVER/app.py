4854904dd56532d8f0b2770ee14b9f1f73f272db
Rahul <rahul.agarwal4@learner.manipal.edu>
2019-01-23 22:42:55 +0530

    gui and fiels to run

---
+++ DISCOVER/app.py
@@ -0,0 +1,22 @@
+import requests, datetime
+from flask import Flask, render_template, redirect, json, request, session, Markup, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def main():
+	return render_template('home.html')
+
+@app.route('/submit',methods=['POST'])
+def get_recommendations():
+	topic = request.form['topic']
+	title = request.form['title']
+	keywords = request.form['keywords']
+	abstract = request.form['abstract']
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=10002,use_evalex=False)
+    # app.run(debug=True,host='192.168.43.53',port=5007,use_evalex=False)
