15c24898214c74d2469a7a3d33682c9672279e12
Rahul <rahul.agarwal4@learner.manipal.edu>
2019-01-26 22:01:17 +0530

    working prototype I

--- DISCOVER/app.py
+++ DISCOVER/app.py
@@ -2,21 +2,77 @@ import requests, datetime
 from flask import Flask, render_template, redirect, json, request, session, Markup, flash
 
 app = Flask(__name__)
-
-@app.route('/')
+app.secret_key = '8bf9547569cd5a638931a8639cf9f86237931e92' 
+# @app.route('/')
 @app.route('/home')
 def main():
 	return render_template('home.html')
 
-@app.route('/submit',methods=['POST'])
+def get_sample_number(title_new):
+	path = "output/input.txt"
+
+	with open(path,'r') as f:
+		for line in f:
+			line = line.strip("\t|\n")
+			title,sample_num = line.split('<--')
+			if(title == title_new):
+				return sample_num
+	return 0;
+
+def get_results(sample_number):
+	# path = "../output/sample"+str(sample_number)+"/LN"+str(sample_number)
+	path = "output/LN"+str(sample_number)+".txt"
+	html_text=''
+
+	with open(path,'r') as f:
+		i=0
+		for line in f:
+			i+=1
+			line = line.strip("\t|\n")
+			print line
+			rowid,rowvalue = line.split('.')
+			html_text += '<tr class="row100">'
+			html_text += '<td class="column100 column1" data-column="column1">' + rowid + '</td>' 
+			html_text += '<td class="column100 column2" data-column="column2">' + rowvalue + '</td>' 
+			html_text += '</tr>'
+			if(i==15):
+				break
+
+	return html_text
+
+@app.errorhandler(404)
+@app.errorhandler(405)
+def page_not_found(e):
+	print e
+	msg =' There was an error. go to <a href="/home">home</a> page'
+	return render_template('error.html', error=msg)
+
+
+@app.route('/home',methods=['GET','POST'])
 def get_recommendations():
 	topic = request.form['topic']
 	title = request.form['title']
 	keywords = request.form['keywords']
 	abstract = request.form['abstract']
 
+	sample_number = get_sample_number(title)
+	# sample_number=2
+	if(sample_number == 0):
+		print("sample not found")
+		return redirect('/home')
+	print "afv"
+	session['n']=sample_number
+	
+	return redirect('/results')
 
 
+@app.route('/results')
+def results():
+	html_table = get_results(session['n'])
+	# html_table = 'text to be returnredsgdcghdhrsgf'
+	params={}
+	params['rows'] = html_table
+	return render_template('results.html',params = params)
 
 if __name__ == "__main__":
     app.run(debug=True,port=10002,use_evalex=False)
@@ -24,5 +80,5 @@ if __name__ == "__main__":
 
 
 ##############
-# 			Co authorship, graph model, topic clustering, random walk, citation recommendation.
+# 		Co authorship, graph model, topic clustering, random walk, citation recommendation.
 #############
